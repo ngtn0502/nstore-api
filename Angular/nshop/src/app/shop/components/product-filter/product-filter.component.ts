32cc369e133f7c030c1caa5da4c6f84ac2e1e462
NhanNguyen <ngtn0502@gmail.com>
2022-01-16 01:07:59 +0700

    add client angular file

---
+++ Angular/nshop/src/app/shop/components/product-filter/product-filter.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IBrand } from 'src/app/shared/models/brand';
+import { IType } from 'src/app/shared/models/type';
+
+@Component({
+  selector: 'app-product-filter',
+  templateUrl: './product-filter.component.html',
+  styleUrls: ['./product-filter.component.scss'],
+})
+export class ProductFilterComponent implements OnInit {
+  @Input() items: IBrand[] | IType[] | null;
+  @Input() type: string;
+
+  @Output() filterProduct = new EventEmitter<{ id: number; type: string }>();
+  public name: string;
+  public selectedItem: number = 1;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  filterByCategory(id: number) {
+    this.selectedItem = id;
+    const filter = {
+      id: id,
+      type: this.type,
+    };
+    this.filterProduct.emit(filter);
+  }
+}
